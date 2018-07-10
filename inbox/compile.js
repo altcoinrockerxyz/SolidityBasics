20c260a36d23dbee8a4c51e6b907ba32d6244d9c
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-10 12:44:30 +0800

    Corrected error with dirname underscore

--- inbox/compile.js
+++ inbox/compile.js
@@ -3,7 +3,7 @@ const fs = require('fs'); // standard required modules
 const solc = require('solc'); // require solidity compiler to run line 12
 
 
-const inboxPath = path.resolve(_dirname, 'contracts', 'inbox.sol'); // pass three portions of the path
+const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol'); // pass three portions of the path
 // this is so it works in either windows or unix-based systems
 
 const source = fs.readFileSync(inboxPath, 'utf8'); // read in the contents of the file

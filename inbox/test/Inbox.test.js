0906f5965b648a8c1f79be53f3ea3e2f8425cc3b
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-11 05:42:04 +0800

    web3 test fetch generated ETH accounts

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,40 +1,23 @@
 const assert = require('assert');
-const ganache = require('ganache-cli'); // local ethereum network
-const Web3 = require('web3'); // always make it upper case
-// because it's a constructor function and creates instances of web3 library
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
-// The Lower cased web3 is the actual instance of the Web3 constructor above
-// the purpose of each instance is to connect to a specific network
-// we can make multiple instances in one product albeit relatively uncommon
+// REMEMBER: we can only deploy a contract
+// if we have access to an account
 
-// web3 instance needs to be configured by setting up a 'Provider'
-// A provider is a communication layer between the web3 library
-// and some specific ETH network
-// and could be easily exchanged with another provider
-// Keywords: Ganache and Mocha
-// Mocha: Is a test running framework for any type
-// of Javascript code that we want (i.e. front-end app, back-end app, even Eth)
-// Has a very small set of functions that we need to understand
+// define a beforeEach statement
+beforeEach(() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts()
+    // to get access, we have to chain on with the next line
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts); // show in console, check if working
+    });
 
-// test class
-class Car {
-  park() { // Method 1
-    return 'stopped';
-  }
+  // Use one of those accounts to deploy
+  // the contract
+});
 
-  drive() { // Method 2
-    return 'vroom';
-  }
-}
-
-// make a call on a descripe function first
-describe('Car', () => {
-  it('can park', () => { // where we write our actual test setup and assertion logic
-    // Step 1. Create an instance of class Car
-    const car = new Car();
-    // to make assertion use the assert library declared at the top
-    assert.equal(car.park(), 'stopped');
-  });
-}); // first argument string could be any name
-// and not necessary to be name of class
-// second argument is an array function
+describe('Inbox', () => {
+  it('deploys a contract', () => {});
+});

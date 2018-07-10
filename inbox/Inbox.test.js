10a05d32a0a9d8554bd2f3a93031b04ae60a6dab
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-11 04:08:36 +0800

    Ganache and Mocha

--- inbox/Inbox.test.js
+++ inbox/Inbox.test.js
@@ -2,4 +2,16 @@ const assert = require('assert');
 const ganache = require('ganache-cli'); // local ethereum network
 const Web3 = require('web3'); // always make it upper case
 // because it's a constructor function and creates instances of web3 library
-const web3 = require('web3'); // the actual instance of the Web3 constructor above
+const web3 = new Web3(ganache.provider());
+// The Lower cased web3 is the actual instance of the Web3 constructor above
+// the purpose of each instance is to connect to a specific network
+// we can make multiple instances in one product albeit relatively uncommon
+
+// web3 instance needs to be configured by setting up a 'Provider'
+// A provider is a communication layer between the web3 library
+// and some specific ETH network
+// and could be easily exchanged with another provider
+// Keywords: Ganache and Mocha
+// Mocha: Is a test running framework for any type
+// of Javascript code that we want (i.e. front-end app, back-end app, even Eth)
+// Has a very small set of functions that we need to understand

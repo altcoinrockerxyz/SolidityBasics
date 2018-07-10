936064af94ef99625f50594d803b12136bcbb72d
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-10 12:37:36 +0800

    Lesson 33

---
+++ inbox/compile.js
@@ -0,0 +1,14 @@
+const path = require('path'); // standard required modules
+const fs = require('fs'); // standard required modules
+const solc = require('solc'); // require solidity compiler to run line 12
+
+
+const inboxPath = path.resolve(_dirname, 'contracts', 'inbox.sol'); // pass three portions of the path
+// this is so it works in either windows or unix-based systems
+
+const source = fs.readFileSync(inboxPath, 'utf8'); // read in the contents of the file
+
+// write the actual compile statement using Solidity compiler
+console.log(solc.compile(source, 1));
+// pass in the source code and number of different contracts we are attempting to compile
+// wrap in console.log() to show what's happening behind the scene

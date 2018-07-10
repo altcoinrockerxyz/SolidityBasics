46c66fd2ab62dd92dbeb449dd9512c0dc9d8cb01
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-10 15:26:31 +0800

    Show only bytecode and interface

--- inbox/compile.js
+++ inbox/compile.js
@@ -9,7 +9,13 @@ const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol'); // pass thr
 const source = fs.readFileSync(inboxPath, 'utf8'); // read in the contents of the file
 
 // write the actual compile statement using Solidity compiler
-console.log(solc.compile(source, 1));
-solc.compile(source, 1);
+// console.log(solc.compile(source, 1));
+// module.exports = solc.compile(source, 1);
 // pass in the source code and number of different contracts we are attempting to compile
 // wrap in console.log() to show what's happening behind the scene
+
+// whenever we compile, make sure it could be accessed by other files
+// immediately get access to compiled source code
+// we only need to pull out the Inbox property
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
+// this gives direct access to just the bytecode and interface property

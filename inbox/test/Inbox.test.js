82831249590772dfbf0f310ec800dfecb17258ef
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-11 05:47:41 +0800

    Replace promise statement w/ async await to cleanup code

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -5,19 +5,23 @@ const web3 = new Web3(ganache.provider());
 // REMEMBER: we can only deploy a contract
 // if we have access to an account
 
+let accounts;
+
 // define a beforeEach statement
-beforeEach(() => {
+beforeEach(async () => {
   // Get a list of all accounts
-  web3.eth.getAccounts()
-    // to get access, we have to chain on with the next line
-    .then(fetchedAccounts => {
-      console.log(fetchedAccounts); // show in console, check if working
-    });
+
+  // the .then function is a promise function
+  // rather than use a promise function,
+  // we are going to use async await syntax
+  accounts = await web3.eth.getAccounts();
 
   // Use one of those accounts to deploy
   // the contract
 });
 
 describe('Inbox', () => {
-  it('deploys a contract', () => {});
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
 });

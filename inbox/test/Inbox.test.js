110e2876986e94d178ad76fee23da230a8e2c02a
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-16 12:13:17 +0800

    Lesson 48: Verifying the Initial Message

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,7 +3,12 @@ require('events').EventEmitter.defaultMaxListeners = 0;
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider());
+
+// Update via Lecture 47
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+
+// const web3 = new Web3(ganache.provider());
 // REMEMBER: we can only deploy a contract
 // if we have access to an account
 
@@ -30,7 +35,7 @@ beforeEach(async () => {
     // deploy - pass in some arguments, number of arguments depend on number of variables on the created Class/function
     // simply put, this deploys a new copy of the Contract
     .deploy({ data: bytecode, arguments: ['Hi there!'] }) // a method we chain on to the object that is returned from the contract on line 26
-    .send({ from: accounts [0], gas: '1000000' }) // account at array zero or first account, then specify the gas
+    .send({ from: accounts [0], gas: '1000000' }); // account at array zero or first account, then specify the gas
 
     // Whenever we deploy this async operation contract, the value that is returned
     // from all these (await, deploy and send),
@@ -38,6 +43,8 @@ beforeEach(async () => {
     // inbox is our JavaScript representation of the contract
     // it represents what exists on the blockchain
 
+    // added via Lecture 47
+    inbox.setProvider(provider);
 });
 
 // test to make sure the contract is successfully deployed
@@ -46,5 +53,14 @@ describe('Inbox', () => {
   it('deploys a contract', () => {
     assert.ok(inbox.options.address); // checks if the inbox.options.address value is defined or true
     // console.log(inbox);
+  });
+
+  it('has a default message', async () => {
+    // call a method on our inbox contract but not modify any data
+    // assume it will return a promise -async-
+    const message = await inbox.methods.message().call();
+    // we are calling message method, not setMessage
+    // next step is to assert the value of the message
+    assert.equal(message, 'Hi there!')
   });
 });

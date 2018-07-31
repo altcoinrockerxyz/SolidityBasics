d30b8403070fb74e19a49a88112a8f51dd1a891b
altcoinrockerxyz <sam@oceanmoon.rocks>
2018-07-31 20:41:08 +0800

    Lecture 81: Test for multiple entries

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -31,10 +31,22 @@ describe('Lottery Contract', () => {
     assert.ok(lottery.options.address);
   });
 
-
-  it('allows one account to enter', async () => {
+  // check if it accepts entries
+  it('allows multiple accounts to enter', async () => {
     await lottery.methods.enter().send ({
       from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether'),
+      gas: '1000000' // added based on https://github.com/trufflesuite/truffle/issues/748
+    });
+
+    await lottery.methods.enter().send ({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether'),
+      gas: '1000000' // added based on https://github.com/trufflesuite/truffle/issues/748
+    });
+
+    await lottery.methods.enter().send ({
+      from: accounts[2],
       value: web3.utils.toWei('0.02', 'ether'),
       gas: '1000000' // added based on https://github.com/trufflesuite/truffle/issues/748
     });
@@ -44,8 +56,9 @@ describe('Lottery Contract', () => {
     });
 
     assert.equal(accounts[0], players[0]);
-    assert.equal(1, players.length);
-
+    assert.equal(accounts[1], players[1]);
+    assert.equal(accounts[2], players[2]);
+    assert.equal(3, players.length);
 
   });  // end it
 
